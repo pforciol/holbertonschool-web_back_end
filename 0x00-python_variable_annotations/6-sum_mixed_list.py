21126afd4d37529fafbcc5de9a660f02701288a5
Pierre Forcioli <2500@holbertonschool.com>
2021-10-19 06:35:01 +0000

    Fix error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,10 +5,10 @@ This module contains a type-annotated function.
 
 """
 
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
 
     Args:
