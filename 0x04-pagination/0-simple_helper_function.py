b961849a20b896f60c96b8acd3ac0c4a269c603a
Pierre Forcioli <2500@holbertonschool.com>
2021-10-27 09:08:46 +0000

    Fix hashbang import

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ index_range module """
 
 from typing import Tuple
--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ hypermedia_pagination module """
 
 import csv
