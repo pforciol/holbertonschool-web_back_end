3bc2e96bebb1e100ca63202b6647d65571c67b71
Pierre Forcioli <2500@holbertonschool.com>
2021-11-02 09:47:56 +0000

    Add 3 first tasks

---
+++ 0x05-personal_data/README.md
@@ -0,0 +1,6 @@
+# 0x05. Personal Data
+
+-   Examples of Personally Identifiable Information (PII)
+-   How to implement a log filter that will obfuscate PII fields
+-   How to encrypt a password and check the validity of an input password
+-   How to authenticate to a database using environment variables
---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+""" filtered_logger module """
+
+import logging
+import re
+from typing import List
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    """
+
+        Args:
+            fields: all fields to obfuscate
+            redaction: by what the field will be obfuscated
+            message: the log line
+            separator: by which character separate all fields in the message
+
+        Returns:
+            The log message, obfuscated.
+
+    """
+
+    for field in fields:
+        message = re.sub(fr'{field}=([^=]*){separator}',
+                         fr'{field}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+
+        Redacting Formatter class
+
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+
+        Returns:
+            The Logger object.
+
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.addHandler(ch)
+
+    return logger
