9d7b6ad9d3ddd912d91de1a4e6dff7abcfb6c791
Piroli <pforciol@student.42.fr>
2021-12-14 10:45:39 +0000

    Fix 6

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -5,5 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((values) => {
+    return values;
+  });
 }
