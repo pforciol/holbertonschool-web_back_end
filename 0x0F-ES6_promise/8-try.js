40fdc0515e649e5d374f499410c582d8fe1dafad
Piroli <pforciol@student.42.fr>
2021-12-14 10:54:13 +0000

    Clean error message and ===

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,5 +1,5 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator == 0) {
+  if (denominator === 0) {
     throw Error('cannot divide by 0');
   } else {
     return numerator / denominator;
--- 0x0F-ES6_promise/9-try.js
+++ 0x0F-ES6_promise/9-try.js
@@ -1,10 +1,10 @@
 export default function guardrail(mathFunction) {
-  let queue = [];
+  const queue = [];
 
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push(error);
+    queue.push(`${error.name}: ${error.message}`);
   }
 
   queue.push('Guardrail was processed');
