545b206c047455f6e1dc21e4e22b8615b9a302af
Piroli <pforciol@student.42.fr>
2021-12-14 10:39:21 +0000

    fix missing return

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const { body } = values[0];
       const { firstName, lastName } = values[1];
