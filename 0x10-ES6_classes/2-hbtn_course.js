a456034edf2c1f5fc3549e7a7de91167b5d3fbb1
Piroli <pforciol@student.42.fr>
2021-12-16 09:29:21 +0000

    Fix lint for 2

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -8,25 +8,29 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   set name(value) {
-    if (typeof value != 'string') throw TypeError('Name must be a string');
+    if (typeof value !== 'string') throw TypeError('Name must be a string');
     this._name = value;
   }
 
   get length() {
     return this._length;
   }
+
   set length(value) {
-    if (typeof value != 'number') throw TypeError('Length must be a number');
+    if (typeof value !== 'number') throw TypeError('Length must be a number');
     this._length = value;
   }
 
   get students() {
     return this._students;
   }
+
   set students(value) {
-    if (!(value instanceof Array))
+    if (!(value instanceof Array)) {
       throw TypeError('Students must be an array of strings');
+    }
     this._students = value;
   }
 }
