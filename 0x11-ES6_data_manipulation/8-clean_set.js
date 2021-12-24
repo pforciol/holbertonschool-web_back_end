d97c0c35d13a7866973b9dbbaa69e5b0072afa13
Piroli <pforciol@student.42.fr>
2021-12-24 11:42:58 +0000

    Fix 8

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!startString && typeof startString !== 'string') return '';
 
   return [...set]
     .filter((str) => typeof str === 'string' && str.startsWith(startString))
