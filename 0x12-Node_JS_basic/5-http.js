d3f5d027858cd64661ba87b8b1e51b9cc88578d5
Piroli <pforciol@student.42.fr>
2021-12-31 15:09:16 +0000

    Add task 5

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,29 +4,32 @@ const countStudents = require('./3-read_file_async');
 const host = 'localhost';
 const port = 1245;
 
-const requestListener = async function (req, res) {
+const requestListener = async (req, res) => {
   switch (req.url) {
     case '/students':
       res.writeHead(200);
-      const { students, fields } = await countStudents('database.csv');
+      try {
+        const { students, fields } = await countStudents(process.argv[2]);
+        res.write(`Number of students: ${students.length}\n`);
+        for (const student of students) {
+          fields.add(student.field);
+        }
 
-      res.write(`Number of students: ${students.length}\n`);
-      for (const student of students) {
-        fields.add(student.field);
+        for (const field of fields) {
+          const data = students
+            .filter((s) => s.field === field)
+            .map((s) => s.firstname);
+
+          res.write(
+            `Number of students in ${field}: ${data.length}. List: ${data.join(
+              ', ',
+            )}\n`,
+          );
+        }
+        res.end();
+      } catch (err) {
+        res.end('Error: Cannot load the database');
       }
-
-      for (const field of fields) {
-        const data = students
-          .filter((s) => s.field === field)
-          .map((s) => s.firstname);
-
-        res.write(
-          `Number of students in ${field}: ${data.length}. List: ${data.join(
-            ', ',
-          )}\n`,
-        );
-      }
-      res.end();
       break;
     default:
       res.writeHead(200);
